9c605668008e60284157be2d11fd95832ce1c1cd
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2023-12-08 14:50:30 +0100

    day 2 finished

--- 2023/code/Day2.py
+++ 2023/code/Day2.py
@@ -35,3 +35,34 @@ for line in single_line:
         sum += int(id[0])
 
 print(f"solution part 1: {sum}")
+
+sum_pt2 = 0
+
+for line in single_line:
+    games = line.split(":")[1]
+    blue_count, red_count, green_count = 0, 0, 0
+    final_blue_count, final_red_count, final_green_count = 0, 0, 0
+    single_games = games.split(";")
+
+    for curr_game in single_games:
+        try:
+            green_count = int(re.findall(green, curr_game)[0])
+        except:
+            green_count = 0
+        try:
+            blue_count = int(re.findall(blue, curr_game)[0])
+        except:
+            blue_count = 0
+        try:
+            red_count = int(re.findall(red, curr_game)[0])
+        except:
+            red_count = 0
+        if green_count > final_green_count:
+            final_green_count = green_count
+        if red_count > final_red_count:
+            final_red_count = red_count
+        if blue_count > final_blue_count:
+            final_blue_count = blue_count
+    sum_pt2 += (final_blue_count * final_red_count * final_green_count)
+
+print(f"Solution pt 2: {sum_pt2}")
