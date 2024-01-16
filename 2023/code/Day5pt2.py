7273c54aa2d2a8bd40b158fba3f14175540ad46e
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2024-01-16 13:32:56 +0100

    day 5 finished

--- 2023/code/Day5pt2.py
+++ 2023/code/Day5pt2.py
@@ -1,5 +1,38 @@
 data = open("2023\inputs\day5.txt", "r").read()
+# data = """seeds:
+# 79 14 55 13
 
+# seed-to-soil map:
+# 50 98 2
+# 52 50 48
+
+# soil-to-fertilizer map:
+# 0 15 37
+# 37 52 2
+# 39 0 15
+
+# fertilizer-to-water map:
+# 49 53 8
+# 0 11 42
+# 42 0 7
+# 57 7 4
+
+# water-to-light map:
+# 88 18 7
+# 18 25 70
+
+# light-to-temperature map:
+# 45 77 23
+# 81 45 19
+# 68 64 13
+
+# temperature-to-humidity map:
+# 0 69 1
+# 1 0 69
+
+# humidity-to-location map:
+# 60 56 37
+# 56 93 4"""
 
 separated_blocks = data.split("\n\n")
 
@@ -17,66 +50,49 @@ for i in range(len(seeds_list)):
 
 seeds_list = total_seeds_list.copy()
 
-curr_min = float("inf")
-
 separated_blocks = separated_blocks[1:]
-
-print("here we go6")
-
-# for seeds_range in seeds_list:
-#     print("new range")
-#     #new_seedlist = list(range(seeds_range[0], seeds_range[0]+seeds_range[1]))
-#     for block in separated_blocks:
-#         # new_seedlist = []
-#         epic_seeds = []
-#         block = block.split("\n")[1:]
-#         temp_seedlist = new_seedlist.copy()
-#         for line in block:
-#             end, start, total_range = [int(x) for x in line.split(" ")]
-#             difference = end - start
-#             for j in new_seedlist:
-#                 if j >= start and j < (start + total_range):
-#                     epic_seeds.append(int(j + difference))
-#                     temp_seedlist.remove(j)
-                
-#         if len(temp_seedlist) > 0:
-#             epic_seeds += temp_seedlist
-#         new_seedlist = epic_seeds.copy()
-#     if sorted(epic_seeds[0]) < curr_min:
-#         curr_min = sorted(epic_seeds[0])
-
 
 
 for block in separated_blocks:
     block = block.split("\n")[1:]
-    print(block)
     collecting_seeds = []
     ranges_to_go = seeds_list.copy()
+    next_line_ranges = []
     for line in block:
-        end, start, total_range = [int(x) for x in line.split(" ")]
-        delta = end - start
-        for range in ranges_to_go:
-            range_start = range[0]
-            range_end = range[0] + range[1]
+        trans_goal, trans_start, trans_range = [int(x) for x in line.split(" ")]
+        delta = trans_goal- trans_start
+        trans_end = trans_start + trans_range -1
+        while len(ranges_to_go) > 0:
+            current_range = ranges_to_go.pop(0)
+        # for range in ranges_to_go:
+            # print(range)
+            range_start = current_range[0]
+            range_end = current_range[0] + current_range[1] - 1
             #complete
-            if range_start >= start and range_end <= start + delta:
-                collecting_seeds.append([range_start+delta, range_end+delta])
+            if trans_start <= range_start and range_end <= trans_end:
+                collecting_seeds.append([range_start+delta, current_range[1]])
             #partial left
-            elif range_start >= start and range_end >= start + delta:
-                collecting_seeds.append([range_start+delta, start + delta - range_start + delta])
-                ranges_to_go.append([start + delta + 1, range_end - start + delta + 1])
+            elif range_start > trans_start and range_start <= trans_end and range_end > trans_end:
+                collecting_seeds.append([range_start+delta, trans_end - range_start + 1])
+                ranges_to_go.append([trans_end + 1, range_end - trans_end])
             #partial right
-            elif range_start <= start and range_end <= start + delta:
-                collecting_seeds.append([start + delta, range_end - start + delta])
-                ranges_to_go.append([range_end + 1, start + delta - range_end + 1])
+            elif range_end >= trans_start and range_end < trans_end and range_start < trans_start:
+                collecting_seeds.append([trans_start + delta, range_end - trans_start])
+                ranges_to_go.append([range_start, trans_start - 1 - range_start])
             #partial inner
-            elif range_start < start and range_end > start + delta:
-                collecting_seeds.append([range_start + delta, range_end + delta])
-                ranges_to_go.append([range_start, start - range_start])
-                ranges_to_go.append([start + delta + 1, range_end - start + delta + 1])
+            elif range_start < trans_start and range_end > trans_end:
+                collecting_seeds.append([trans_start + delta, trans_end-trans_start])
+                ranges_to_go.append([range_start, trans_start - 1 - range_start])
+                ranges_to_go.append([trans_end + 1, range_end - trans_end])
             else:
-                collecting_seeds.append([range_start, range_end])
-    seeds_list = collecting_seeds.copy()
+                next_line_ranges.append([range_start, current_range[1]])
+        ranges_to_go += next_line_ranges.copy()
+        next_line_ranges = []
+    if len(collecting_seeds) == 0:
+        pass
+    else:
+        seeds_list = collecting_seeds.copy() + ranges_to_go.copy()
 
-print(seeds_list)
-print(curr_min)
+print("solution pt 2:", sorted(seeds_list, key=lambda x: x[0])[0][0])
+
+# 34039469 is right
