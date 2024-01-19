41d360ede55a9daf9fc87d12f03e04f0406e943a
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2024-01-19 02:16:20 +0100

    day 7 finished

--- 2023/code/Day7.py
+++ 2023/code/Day7.py
@@ -53,3 +53,68 @@ for index, score in enumerate(valued_cards):
     total += int(score[1]) * (index+1)
 
 print("Solution pt 1:", total)
+#250254244
+
+
+all_cards_split = [x.split(" ") for x in all_cards]
+
+total = 0
+
+valued_cards = all_cards_split.copy()
+
+for index, card in enumerate(all_cards_split):
+    curr_number = ""
+    card_dict = defaultdict(lambda: 0)
+    values = list(card[0])
+    for single in values:
+        card_dict[single] += 1
+    j_count = int(card_dict["J"])
+    del card_dict["J"]
+    listed_dict = list(map(list, card_dict.items()))
+    listed_dict.sort(key=lambda x: x[1])
+    if j_count != 5:
+        listed_dict[-1][1] += j_count
+    else:
+        listed_dict = [["J", 5]]
+    after_J_values = [x[1] for x in listed_dict]
+    match after_J_values:
+        case [5]:
+            curr_number = "7"
+        case [1,4]:
+            curr_number = "6"
+        case [2,3]:
+            curr_number = "5"
+        case [1,1,3]:
+            curr_number = "4"
+        case [1,2,2]:
+            curr_number = "3"
+        case [1,1,1,2]:
+            curr_number = "2"
+        case _:
+            curr_number = "1"
+    for highest_card in values:
+        match highest_card:
+            case "A":
+                curr_number += "14"
+            case "K":
+                curr_number += "13"
+            case "Q":
+                curr_number += "12"
+            case "J":
+                curr_number += "01"
+            case "T":
+                curr_number += "10"
+            case _:
+                curr_number += "0" + str(highest_card)
+    valued_cards[index].append(curr_number)
+
+valued_cards.sort(key=lambda x: x[2])
+
+
+for index, score in enumerate(valued_cards):
+    total += int(score[1]) * (index+1)
+
+
+print("Solution pt 2:", total)
+
+#250087440
