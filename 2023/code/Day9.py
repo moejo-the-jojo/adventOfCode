61d41fc219bea0e396248aa135b3ccba6eab4283
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2024-03-05 22:49:34 +0100

    changes go to laptop

--- 2023/code/Day9.py
+++ 2023/code/Day9.py
@@ -7,30 +7,23 @@ int_lines = []
 for i in lines:
     int_lines.append([int(x) for x in i])
 
-# print(int_lines)
-
 sum = 0
 
 for original_line in int_lines:
     calculation_list = [original_line]
-    zauber = 0
-    while True:
-        if calculation_list[-1][0] == 0 and calculation_list[-1][1] == 0:
-            break
+    while calculation_list[-1][0] != 0 or calculation_list[-1][1] != 0:
         temp = []
-        for element in range(len(calculation_list[-1])-1):
-            temp.append(calculation_list[-1][element+1] - calculation_list[-1][element])
+        for number_in_list in range(len(calculation_list[-1])-1):
+            temp.append(calculation_list[-1][number_in_list+1] - calculation_list[-1][number_in_list])
         calculation_list.append(temp)
-
-
+    
     while len(calculation_list) > 1:
         last_one = calculation_list.pop()
         calculation_list[-1].append(calculation_list[-1][-1] + last_one[-1])
 
-    print(calculation_list[-1][-1])
+
     sum += calculation_list[-1][-1]
 
-print(sum)
+print("Solution: " + str(sum))
 
-#1782889025 too high
-#1744890533 too low
+#"1782889025" is too high
