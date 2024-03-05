414359759269935974126c5b45cab5d55f3eff72
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2024-03-05 23:45:07 +0100

    made functions on day 9

--- 2023/code/Day9.py
+++ 2023/code/Day9.py
@@ -1,29 +1,45 @@
-data = open("2023/inputs/day9.txt", "r").read()
+# get data and process it
+def get_data(filename):
+    data = open(filename, "r").read()
 
-lines = data.split("\n")
-lines = list(map(lambda x: x.split(" "), lines))
+    split_in_lines = data.split("\n")
+    split_each_line_in_numbers = list(map(lambda x: x.split(" "), split_in_lines))
+    ever_number_is_an_int_now = [[int(x) for x in pre_int_line] for pre_int_line in split_each_line_in_numbers]
 
-int_lines = []
-for i in lines:
-    int_lines.append([int(x) for x in i])
+    return ever_number_is_an_int_now
 
-sum = 0
-
-for original_line in int_lines:
-    calculation_list = [original_line]
+# calculate the next number in a list
+def calculate_next_number(history):
+    # create new lists with differences of last list until the appended list contains only 0
+    calculation_list = [history]
     while calculation_list[-1][0] != 0 or calculation_list[-1][1] != 0:
         temp = []
         for number_in_list in range(len(calculation_list[-1])-1):
             temp.append(calculation_list[-1][number_in_list+1] - calculation_list[-1][number_in_list])
         calculation_list.append(temp)
     
+    # add next number to all lists of differences, and finally to original list
     while len(calculation_list) > 1:
-        last_one = calculation_list.pop()
-        calculation_list[-1].append(calculation_list[-1][-1] + last_one[-1])
+        last_list = calculation_list.pop()
+        last_number_in_last_list = last_list[-1]
+        calculation_list[-1].append(calculation_list[-1][-1] + last_number_in_last_list)
+    
+    # return the result
+    return calculation_list[-1][-1]
+
+# add numbers to sum
+def sum_results(result):
+    global sum
+    sum += result
 
 
-    sum += calculation_list[-1][-1]
+all_lines = get_data("2023/inputs/day9.txt")
+sum = 0
 
-print("Solution: " + str(sum))
+for single_line in all_lines:
+    next_value = calculate_next_number(single_line)
+    sum_results(next_value)
 
-#"1782889025" is too high
+print(f"Solution: {sum}")
+
+# 1782889025 is too high
